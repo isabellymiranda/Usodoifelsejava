863e352dabd20d986d2797f9653a8a7c84c11de4
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-07 17:56:56 +0000

    subir

---
+++ Usoifeelse.java
@@ -0,0 +1,15 @@
+public class Usoifeelse {
+    public static void main (String[] args) {
+        int valor1 = 1;
+        int valor2 = 2;
+
+        System.out.println((valor1 > valor2 ? "1 é Maior" : "2 é Maior"));
+
+        if (valor1 > valor2) {
+            System.out.println("1 é Maior");
+        } else {
+            System.out.println("2 é Maior");
+        }
+        
+    }
+}
