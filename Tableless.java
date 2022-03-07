f505458b8ee05f505bb14cfc5581af2982532486
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-07 19:12:40 +0000

    subir

---
+++ Tableless.java
@@ -0,0 +1,22 @@
+public class Tableless {
+    public static void main(String[]args){
+        int valor =0;
+        switch (valor) {
+            case 1:
+            System.out.println("Valor =1");
+            break;
+            case 2:
+            System.out.println("Valor =2");
+            break;
+            case 3:
+            System.out.println("Valor =3");
+            break;
+            default:
+            System.out.println("Nenhum dos valores");
+
+
+
+        }
+    }
+    
+}
